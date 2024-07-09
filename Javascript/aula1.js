f09b126245210e432b181ce01ef3f932be1e3211
Daniela Gomes <stherffanygomesz@gmail.com>
2024-07-08 20:18:58 -0400

    javascript

---
+++ Javascript/aula1.js
@@ -0,0 +1,32 @@
+// const aluno = 'dani';
+// if(aluno == 'dani'){
+//     console.log('Daniela');
+// }
+// else{
+//     console.log('NAO')
+// }
+
+// const numero = 40; //para comparar duas coisas é preciso colocar três ===
+// if(numero === '22'){ 
+//     console.log('Ele é igual a 22');
+// }
+// else{
+//     console.log('Ih, é igual não');
+// }
+
+// if(30 > numero){
+//     console.log('É maior');
+// }
+// else if(30 != numero){
+//     console.log('É diferente');
+// }
+
+const numero = 5;
+
+if(numero > 2 && numero < 6){
+    console.log('Ele esta nesse intervalo');
+}
+if(numero > 2 || numero > 6){
+    console.log('Ele esta nesse intervalo de novo!');
+}
+
