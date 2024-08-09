bcc73ec931f51314c17b551b4d2959547fce85e9
Daniela Gomes <stherffanygomesz@gmail.com>
2024-08-09 18:50:32 -0400

    nodejs

--- Back/Nodejs/index.js
+++ Back/Nodejs/index.js
@@ -1,6 +1,24 @@
 const express = require('express')
 
 const app = express();
+app.use(express.json())
+
+const users = [
+    {id: 1, name: "Daniela"},
+    {id: 2, name: "Felipe"}
+]
+//app.get recebe dois parâmetros, uma rota e oq eu quero que acontteça quando eu acessar
+app.get('/usuarios', (req, res) =>{
+    res.json(users);
+})
+
+app.post('/usuarios', (req, res) =>{
+    const name = req.body.name;
+    const id = users.length+1;
+    const newUser = {id, name}
+    users.push(newUser);
+    res.status(201).json(newUser);
+})
 
 app.get('/', (req,res) => {
     res.send({message: "Olá Mundo"})
