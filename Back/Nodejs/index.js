1f563af6176bef6ca43b953459f9601f093d5dc2
Daniela Gomes <stherffanygomesz@gmail.com>
2024-08-09 20:13:24 -0400

    usuarios nodejs

--- Back/Nodejs/index.js
+++ Back/Nodejs/index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 
 const app = express();
+
 app.use(express.json())
 
-const users = [
+let users = [
     {id: 1, name: "Daniela"},
-    {id: 2, name: "Felipe"}
+    {id: 2, name: "Felipe"},
 ]
 //app.get recebe dois parâmetros, uma rota e oq eu quero que acontteça quando eu acessar
 app.get('/usuarios', (req, res) =>{
-    res.json(users);
+    res.status(200).json(users);
 })
 
 app.post('/usuarios', (req, res) =>{
@@ -18,6 +19,24 @@ app.post('/usuarios', (req, res) =>{
     const newUser = {id, name}
     users.push(newUser);
     res.status(201).json(newUser);
+})
+
+app.put("/usuarios/:id", (req,res) => { //passando um id para a rota
+    const id = parseInt(req.params.id)
+    const {name} = req.body
+    const userIndex = users.findIndex(user => user.id == id);
+    if(userIndex != -1){
+        users[userIndex].name = name;
+        res.json(users[userIndex])
+    } else{
+        res.status(400).json({message: 'Usuário não encontrado'})
+    }
+})
+
+app.delete('/usuarios/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    users = users.filter(user => user.id !== id);
+    res.sendStatus(204)
 })
 
 app.get('/', (req,res) => {
